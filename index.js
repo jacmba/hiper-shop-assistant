3666ced259e36e3f9d75eaf7304e305d7a651308
Jacinto Mba Cantero <jacmba@gmail.com>
2022-09-14 17:02:30 +0100

    Changed target store. Successful client login

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Browser, Builder, By, Key, until } = require("selenium-webdriver");
 const b64 = require("crypto-js/enc-base64");
 const CryptoJS = require("crypto-js");
+const { Options } = require("selenium-webdriver/firefox");
 
 const credentials = process.env.SHOP_CREDENTIALS;
 if(!credentials) {
@@ -16,20 +17,20 @@ if(!decoded.includes(separator)) {
 [username, password] = decoded.split(separator);
 
 (async () => {
-  const driver = await new Builder().forBrowser("firefox").build();
+  const options = new Options();
+  options.setPreference("general.useragent.override", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36");
+  const driver = await new Builder()
+    .forBrowser("firefox")
+    .setFirefoxOptions(options)
+    .build();
 
   try {
-    await driver.get("https://www.hipercor.es/supermercado/");
-    await driver.findElement(By.id('cookies-agree-all')).click();
-    await driver.sleep(1000);
-    await driver.findElement(By.className('ts-login-desktop')).click();
-    await driver.sleep(1000);
-    await driver.findElement(By.id('login')).sendKeys(username);
-    await driver.findElement(By.id('password')).sendKeys(password);
-    await driver.findElement(By.id('cookies-agree-all')).click();
-    await driver.sleep(2000);
-    await driver.findElement(By.id('login-btn')).click();
-    await driver.sleep(10000);
+    await driver.get('https://www.hiperdino.es');
+    await driver.findElement(By.id('btn-cookie-allow--all')).click();
+    await driver.findElement(By.className('icon-user')).click();
+    await driver.findElement(By.name('login[username]')).sendKeys(username);
+    await driver.findElement(By.name('login[password]')).sendKeys(password, Key.RETURN);
+    await driver.sleep(5000);
   } finally {
     driver.quit();
   }
