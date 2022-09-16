f01cca1c80807f46cac268ce2f7532913363014a
Jacinto Mba Cantero <jacmba@gmail.com>
2022-09-16 13:12:57 +0100

    Headless mode browser. Added remaining endpoints

--- src/api.js
+++ src/api.js
@@ -2,7 +2,8 @@ const logger = require('./logging');
 
 let app, driver;
 
-const login = async(req, res) => {
+const login = async (req, res) => {
+  logger.info('Requested login from ' + req.ip);
   try {
     await driver.login();
     logger.info('Login success');
@@ -13,8 +14,11 @@ const login = async(req, res) => {
   }
 }
 
-const search = async(req, res) => {
+const search = async (req, res) => {
   const name = req.query.name;
+  if (!name) {
+    return res.status(400).send('Missing "name" param');
+  }
   try {
     const result = await driver.find(name);
     logger.debug(`Return search ${"name"}`, result);
@@ -25,12 +29,51 @@ const search = async(req, res) => {
   }
 }
 
+const add = async (req, res) => {
+  const name = req.query.name;
+  const amount = req.query.amount;
+  if (!name || !amount) {
+    return res.status(400).send('Missing params. Required "name" and "amount"');
+  }
+  try {
+    await driver.add(name, amount);
+    return res.json({
+      item: name,
+      amount,
+      added: 'OK'
+    });
+  } catch (e) {
+    logger.error('Error adding item', e);
+    return res.status(409).send(e);
+  }
+}
+
+const showCart = async (req, res) => {
+  try {
+    const result = await driver.showCart();
+    return res.json(result);
+  } catch (e) {
+    logger.error('Error showing cart', e);
+    return res.status(409).send(e);
+  }
+}
+
+const logout = async (req, res) => {
+  driver.shutdown();
+  return res.json({
+    logout: 'OK'
+  });
+}
+
 const init = (a, d) => {
   app = a;
   driver = d;
 
   app.get('/login', login);
   app.get('/search', search);
+  app.get('/add', add);
+  app.get('/cart', showCart);
+  app.get('/logout', logout);
 }
 
 module.exports = {init};
--- src/driver.js
+++ src/driver.js
@@ -28,6 +28,7 @@ const init = async () => {
     "general.useragent.override",
     "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"
   );
+  options.addArguments('--headless');
   driver = await new Builder()
     .forBrowser("firefox")
     .setFirefoxOptions(options)
